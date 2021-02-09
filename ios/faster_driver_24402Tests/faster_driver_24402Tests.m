110e2cbc40f534796a29c3b2964d6eaa397209a3
Crowdbotics <team@crowdbotics.com>
2021-02-09 16:48:08 +0000

    Initial commit.

---
+++ ios/faster_driver_24402Tests/faster_driver_24402Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface faster_driver_24402 : XCTestCase
+
+@end
+
+@implementation faster_driver_24402
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
